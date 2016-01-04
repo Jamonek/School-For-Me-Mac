426a1c1a04c59b573eb68f53e3d644e81f9f8050
Jamone Kelly <xg.monie089@gmail.com>
2016-01-04 17:27:24 -0500

    Initial Commit

---
+++ School For Me (OS X)/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  School For Me (OS X)
+//
+//  Created by Jamone Alexander Kelly on 1/4/16.
+//  Copyright © 2016 Jamone Kelly. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
